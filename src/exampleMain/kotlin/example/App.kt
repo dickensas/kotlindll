7971e0dcca88fa42a95992857211d3669da7aa31
Dickens A S <dickens.as@gmail.com>
2020-04-20 18:05:41 +0530

    kotlin dll

--- src/exampleMain/kotlin/example/App.kt
+++ src/exampleMain/kotlin/example/App.kt
@@ -2,9 +2,23 @@ package example
 
 import extlib.*
 import kotlinx.cinterop.*
-   
+ 
+typealias SomeFun = CFunction<() -> CPointer<ByteVar>?>
+
 fun main(args: Array<String>) {
 
+	val lib = memScoped {
+	   alloc<libmyext_ExportedSymbols>().apply {
+          libmyext_symbols()
+       }
+    }
+    
+    val s = lib.kotlin.root.example.someLibraryMethod?.reinterpret<SomeFun>()
+    
+    //FIXME: prints null
+	println(s?.invoke())
+	
+	//C stub from .def file - works fine
 	println(getStringFromLib()!!.toKString());
 	
 }
